8947f0a11a6b0c9f0fcc5b08bf3bd4704041c340
Ahmad Noureddine <Ahmadnnoureddine@gmail.com>
2017-01-11 18:43:12 +0200

    ATM Deposit Menu Fix
    ATM Deposit Menu Fix

---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label2.Text = "Amount Deposited"
+        TextBox1.Enabled = False
+
+    End Sub
+End Class
